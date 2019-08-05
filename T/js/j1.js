c7905d87c052cec5dcdc15d2c59a8c9ffd5ae0e0
Greenbirch2007 <291109028@qq.com>
2019-08-05 15:55:37 +0800

    first commit

---
+++ T/js/j1.js
@@ -0,0 +1,24 @@
+// // $(document).ready(function () {
+// //     $("p").click(function () {
+// //         $(this).hide();
+// //     });
+// // });
+//
+// // $(document).ready(function(){
+// //     $("p").click(function(){
+// //         $(this).hide();
+// //     });
+// // });
+//
+//
+// $("button").click(function(){
+//     $("p").hide("slow",function(){
+//         alert("段落现在被隐藏了");
+//     });
+// });
+//
+// $(document).ready(function(){
+//     $("h2").css("color","red").slideUp(2000).slideDown(2000);
+// });
+//
+$(#date).datepicker();
